61e2810aad04751504737f78699588de11c2961a
Romain Aranda <romain.aranda@gmail.com>
2021-04-30 11:04:48 +0200

    change API key

--- reactapp/src/ScreenSource.js
+++ reactapp/src/ScreenSource.js
@@ -21,7 +21,7 @@ function ScreenSource(props) {
         var country = 'us'
       }
       props.changeLang(selectedLang)
-      const data = await fetch(`https://newsapi.org/v2/sources?language=${langue}&country=${country}&apiKey=b32c8b844d1243b1a7998d8228910f50`)
+      const data = await fetch(`https://newsapi.org/v2/sources?language=${langue}&country=${country}&apiKey=034890f10d404fa4b6f391d59bde530b`)
       const body = await data.json()
       setSourceList(body.sources)
     }
